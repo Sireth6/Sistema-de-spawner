ab5c95dda8993253531cd25ce5b9aedf86ba5aea
Sireth6 <fsvg06@gmail.com>
2023-04-04 15:00:12 -0600

    Se añadió script
    Se añadio un nuevo scrip de moviemiento

---
+++ Sistema de spawner2/Assets/Script/PlayerMotor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    float moveSpeed;
+    float walkSpeed = 2;
+    float sprintSpeed = 6;
+    float gravity = 4 ;
+
+    Vector3 moveDirection;
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController> ();
+    }
+
+    void Update()
+    {
+        Move(); 
+    }
+
+    void Move()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(moveX, 0, moveZ);
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                moveSpeed = sprintSpeed;
+            }
+            else
+            {
+                moveSpeed = walkSpeed;
+            }
+
+            moveDirection *= moveSpeed;
+        }
+
+        moveDirection.y -= gravity;
+        controller.Move (moveDirection * Time.deltaTime);
+    }
+}
