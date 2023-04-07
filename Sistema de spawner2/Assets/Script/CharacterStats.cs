4951a2d0538dcb9a370810ff0fefbf819dee094f
Sireth6 <fsvg06@gmail.com>
2023-04-06 20:00:05 -0600

    Script Stats
    Se creó un script para las stats del player

---
+++ Sistema de spawner2/Assets/Script/CharacterStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int currHealth;
+    public int maxHealth;
+
+    public int currStamina;
+    public int maxStamina;
+
+    public bool isDead = false;
+
+    public void CheckHealth()
+    {
+        if (currHealth >= maxHealth)
+        {
+            currHealth = maxHealth;
+        }
+        if (currHealth <= 0)
+        {
+            currHealth = 0;
+            isDead = true;
+        }
+    }
+    public void CheckStamina()
+    {
+        if(currStamina >= maxStamina)
+        {
+            currStamina = maxStamina;
+        }
+        if(currStamina <= 0)
+        {
+            currStamina = 0;
+        }
+    }
+    public virtual void Die()
+    {
+
+    }
+}
---
+++ Sistema de spawner2/Assets/Script/PlayerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 100;
+        currHealth = maxHealth;
+
+        maxStamina = 100;
+        currStamina = maxStamina;
+    }
+
+    public override void Die ()
+    {
+        Debug.Log("You Died");
+    }
+}
